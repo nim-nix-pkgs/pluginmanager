624eb04abb687514d35b9a69ea4b3c270e35608a
hugosenari <hugosenari@gmail.com>
2022-03-12 04:01:49 -0300

    chore: re index pluginmanager

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''Simple plugin implementation'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-pluginmanager-master.flake = false;
+  inputs.src-pluginmanager-master.owner = "samdmarshall";
+  inputs.src-pluginmanager-master.ref   = "refs/heads/master";
+  inputs.src-pluginmanager-master.repo  = "plugin-manager";
+  inputs.src-pluginmanager-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-pluginmanager-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-pluginmanager-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
